b75124f06530b7e0f5e81a3081ac7049e4316101
FrancoisEricMerlin <FrancoisEricMerlin@users.noreply.github.com>
2013-05-15 12:10:34 +0000

    MYLUTECE-31:Move the functionalities adding by the issue Lutece-1580 in the plugin Mylutece
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/authentication/plugin-mylutece@48212 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/mylutece/upgrade/update_db_mylutece-3.0.3-3.0.6.sql
+++ src/sql/plugins/mylutece/upgrade/update_db_mylutece-3.0.3-3.0.6.sql
@@ -1,5 +1,6 @@
 INSERT INTO core_user_right (id_right,id_user) VALUES ('MYLUTECE_MANAGE_AUTHENTICATION_FILTER',1);
 INSERT INTO core_admin_right VALUES ('MYLUTECE_MANAGE_AUTHENTICATION_FILTER', 'mylutece.adminFeature.mylutece_management_authentication_filter.name', 2, 'jsp/admin/plugins/mylutece/security/ManageAuthenticationFilter.jsp', 'mylutece.adminFeature.mylutece_management_authentication_filter.description', 0, 'mylutece', 'USERS', NULL, NULL, NULL);
+INSERT INTO core_admin_role (role_key,role_description) VALUES ('mylutece_manager','Gérer les patramètres avancés Mylutece');
 INSERT INTO core_user_role (role_key,id_user) VALUES ('mylutece_manager',1);
 --
 -- Init Dashboard
