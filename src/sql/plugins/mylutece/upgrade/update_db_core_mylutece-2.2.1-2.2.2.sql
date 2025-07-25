b5f85926e008707e60dc77e3a76da4d31eeb53ff
evdngsl <158455076+evdngsl@users.noreply.github.com>
2025-07-25 11:32:00 +0200

    LUT-30420 : Prepare SQL scripts for Liquibase plugin

--- src/sql/plugins/mylutece/upgrade/update_db_core_mylutece-2.2.1-2.2.2.sql
+++ src/sql/plugins/mylutece/upgrade/update_db_core_mylutece-2.2.1-2.2.2.sql
@@ -1,2 +1,5 @@
+--liquibase formatted sql
+--changeset mylutece:update_db_core_mylutece-2.2.1-2.2.2.sql
+--preconditions onFail:MARK_RAN onError:WARN
 DELETE FROM core_admin_right WHERE id_right = 'MYLUTECE_MANAGE_EXTERNAL_APPLICATION';
 DELETE FROM core_user_right WHERE id_right = 'MYLUTECE_MANAGE_EXTERNAL_APPLICATION';
